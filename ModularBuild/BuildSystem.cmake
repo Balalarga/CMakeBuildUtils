1fe15a8650920cc2c178008080918e25b23e2f19
Balalarga <lightman03@mail.ru>
2023-03-12 23:57:55 +0300

    Add application module

--- ModularBuild/BuildSystem.cmake
+++ ModularBuild/BuildSystem.cmake
@@ -1,58 +1,63 @@
-cmake_minimum_required(VERSION 3.20)
 
-macro(DefineDefaultModule ModuleName SourceDir)
-    file(GLOB_RECURSE PROJECT_SOURCES
-        ${SourceDir}/*.cpp
-        ${SourceDir}/*.h
-        ${SourceDir}/*.hpp)
+macro(DefineDefaultModule ModuleName)
+    cmake_minimum_required(VERSION 3.20)
+    project(${ModuleName} LANGUAGES CXX)
+    file(GLOB_RECURSE PROJECT_SOURCES Source/*.cpp Source/*.h Source/*.hpp)
     add_library(${ModuleName} ${PROJECT_SOURCES})
-    target_include_directories(${ModuleName} PUBLIC ${SourceDir})
+    target_include_directories(${ModuleName} PRIVATE
+        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/Source/${ModuleName}>
+        $<INSTALL_INTERFACE:Source/${ModuleName}>)
+    target_include_directories(${ModuleName} PUBLIC
+        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/Source>
+        $<INSTALL_INTERFACE:Source>)
 endmacro()
 
-macro(AddPublicModuleDependencies ModuleName)
+macro(AddPublicModuleDependency ModuleName)
     set(DependencyModules ${ARGN})
-    message("${ModuleName} public dependencies:")
     foreach(Module ${DependencyModules})
-        message("  ${Module}")
         target_link_libraries(${ModuleName} LINK_PUBLIC ${Module})
         target_include_directories(${ModuleName} PUBLIC ${Module})
     endforeach()
 endmacro()
 
-macro(AddPrivateModuleDependencies ModuleName)
+macro(DefineFolderLibraryWithDeps)
+    cmake_minimum_required(VERSION 3.20)
+    set(CMAKE_CXX_STANDARD 20)
+    set(CMAKE_CXX_STANDARD_REQUIRED ON)
+    get_filename_component(ModuleName ${CMAKE_CURRENT_SOURCE_DIR} NAME)
+    project(${ModuleName} LANGUAGES CXX)
+    file(GLOB_RECURSE PROJECT_SOURCES ${ModuleName}/*.cpp ${ModuleName}/*.h ${ModuleName}/*.hpp)
+    add_library(${ModuleName} ${PROJECT_SOURCES})
+    target_include_directories(${ModuleName} PRIVATE
+        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/${ModuleName}>
+        $<INSTALL_INTERFACE:${ModuleName}>)
+    target_include_directories(${ModuleName} PUBLIC
+        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>
+        $<INSTALL_INTERFACE:>)
     set(DependencyModules ${ARGN})
-    message("${ModuleName} private dependencies:")
     foreach(Module ${DependencyModules})
-        message("  ${Module}")
-        target_link_libraries(${ModuleName} LINK_PRIVATE ${Module})
-        target_include_directories(${ModuleName} PRIVATE ${Module})
+        target_link_libraries(${ModuleName} LINK_PUBLIC ${Module})
+        target_include_directories(${ModuleName} PUBLIC ${Module})
     endforeach()
 endmacro()
 
-macro(DefineFolderNamedModule SourceDir)
+macro(DefineFolderAppWithDeps)
+    cmake_minimum_required(VERSION 3.20)
+    set(CMAKE_CXX_STANDARD 20)
+    set(CMAKE_CXX_STANDARD_REQUIRED ON)
     get_filename_component(ModuleName ${CMAKE_CURRENT_SOURCE_DIR} NAME)
     project(${ModuleName} LANGUAGES CXX)
-    file(GLOB_RECURSE PROJECT_SOURCES
-        ${SourceDir}/*.cpp
-        ${SourceDir}/*.h
-        ${SourceDir}/*.hpp)
-    add_library(${ModuleName} ${PROJECT_SOURCES})
+    file(GLOB_RECURSE PROJECT_SOURCES ${ModuleName}/*.cpp ${ModuleName}/*.h ${ModuleName}/*.hpp)
+    add_executable(${ModuleName} ${PROJECT_SOURCES})
     target_include_directories(${ModuleName} PRIVATE
-        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/${SourceDir}/${ModuleName}>
-        $<INSTALL_INTERFACE:${SourceDir}/${ModuleName}>)
+        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/${ModuleName}>
+        $<INSTALL_INTERFACE:${ModuleName}>)
     target_include_directories(${ModuleName} PUBLIC
-        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/${SourceDir}>
-        $<INSTALL_INTERFACE:${SourceDir}>)
-endmacro()
-
-macro(AddInterfaceModules Target BaseDir)
-    ListOfDirectChildDirectories(MODULES_LIST ${BaseDir})
-    foreach(MODULE ${MODULES_LIST})
-        add_subdirectory(${MODULES_DIR}/${MODULE})
-        target_include_directories(${Target} INTERFACE ${MODULE})
-        target_link_libraries(${Target} INTERFACE ${MODULE})
-        get_property(MODULE_TARGET DIRECTORY ${MODULES_DIR}/${MODULE} PROPERTY BUILDSYSTEM_TARGETS)
-        set_target_properties(${MODULE_TARGET} PROPERTIES FOLDER ${Target})
-        message("Submodule ${MODULE} added to ${Target}")
+        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>
+        $<INSTALL_INTERFACE:>)
+    set(DependencyModules ${ARGN})
+    foreach(Module ${DependencyModules})
+        target_link_libraries(${ModuleName} LINK_PUBLIC ${Module})
+        target_include_directories(${ModuleName} PUBLIC ${Module})
     endforeach()
 endmacro()
